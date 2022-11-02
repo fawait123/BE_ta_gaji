5ebca0b27d16c32a39b1ca3a7bafc2d0c6d43bf7
fawait626 <fawait626@gmail.com>
2022-11-02 21:21:19 +0700

    menambahkan api jabatan

---
+++ app/Http/Controllers/Api/JabatanController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Helpers\Pagination;
+use App\Helpers\Response;
+use App\Http\Controllers\Controller;
+use App\Models\Jabatan;
+use Exception;
+use Illuminate\Http\Request;
+
+class JabatanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        try{
+            $meta = Pagination::defaultMetaInput($request->only(['page','perPage','order','dir','search']));
+            $query = Jabatan::query();
+            $query->where(function($q) use($meta){
+                $q->orWhere('nama', 'like', $meta['search'] . '%');
+            });
+            $total = $query->count();
+            $meta = Pagination::additionalMeta($meta, $total);
+            if ($meta['perPage'] != '-1') {
+                $query->offset($meta['offset'])->limit($meta['perPage']);
+            }
+            $results = $query->get();
+            $data = [
+                'message'  => 'List Data Karyawan',
+                'results'  => $results,
+                'meta'     =>  $meta
+            ];
+            return Response::send(200,$data);
+        }catch(Exception $error){
+            return Response::send(500,$error->getMessage());
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\AbsensiController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\JabatanController;
 use App\Http\Controllers\Api\KaryawanController;
+use App\Http\Controllers\Api\KomponenController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,10 +23,34 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// api karyawan
 Route::group(['prefix'=>'karyawan','as'=>'karyawan'],function(){
     Route::get('/',[KaryawanController::class,'index']);
     Route::post('/',[KaryawanController::class,'store']);
     Route::put('/',[KaryawanController::class,'update']);
     Route::delete('/',[KaryawanController::class,'destroy']);
+});
+
+// api jabatan
+Route::group(['prefix'=>'jabatan','as'=>'jabatan'],function(){
+    Route::get('/',[JabatanController::class,'index']);
+    Route::post('/',[JabatanController::class,'store']);
+    Route::put('/',[JabatanController::class,'update']);
+    Route::delete('/',[JabatanController::class,'destroy']);
+});
+
+// api komponent
+Route::group(['prefix'=>'komponen','as'=>'komponen'],function(){
+    Route::get('/',[KomponenController::class,'index']);
+    Route::post('/',[KomponenController::class,'store']);
+    Route::put('/',[KomponenController::class,'update']);
+    Route::delete('/',[KomponenController::class,'destroy']);
+});
+
+//api absensi
+Route::group(['prefix'=>'absen','as'=>'absen'],function(){
+    Route::get('/',[AbsensiController::class,'index']);
+    Route::post('/',[AbsensiController::class,'store']);
+    Route::put('/',[AbsensiController::class,'update']);
+    Route::delete('/',[AbsensiController::class,'destroy']);
 });
